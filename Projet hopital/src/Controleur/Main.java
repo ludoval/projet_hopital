67a39c6e9971d8c690e1e4bfea6582d2eaf47069
LUDO\ludovic <valery.ludovic@outlook.fr>
2018-04-17 10:26:11 +0200

    module de connexion ajouté

---
+++ Projet hopital/src/Controleur/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+ import vue.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+/**
+ *
+ * @author axel9
+ */
+public class Main {
+    public static void main(String[] args){
+        Login l=new Login();
+        l.setVisible(true);
+    }
+    
+}

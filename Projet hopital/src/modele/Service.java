0a675d83c4212387794dd73e5c3a06eaf1fea359
LUDO\ludovic <valery.ludovic@outlook.fr>
2018-04-17 11:11:22 +0200

    module connexion(base)
    début de dev du module recherche

---
+++ Projet hopital/src/modele/Service.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+import java.util.ArrayList;
+/**
+ * 
+ * @author ludovic
+ */
+public class Service {
+    //attributs
+    private String code;
+    private String nom;
+    private char batiment;
+    private int directeur;
+    private ArrayList<Employe> employes;
+    //getters
+    public String getCode(){return code;}
+    public String getNom(){return nom;}
+    public char getBatiment(){return batiment;}
+    public int getDirecteur(){return directeur;}
+    //public ArrayList getEmploye(){return employes;}
+}

29d1b1fe1cda7f94ad7edae1ef597987fc61792d
AxelGabriel <axel.gabriel@edu.ece.fr>
2018-04-22 17:52:18 +0200

    Version finale du projet

--- Projet hopital/src/modele/Service.java
+++ Projet hopital/src/modele/Service.java
@@ -11,15 +11,26 @@ import java.util.ArrayList;
  */
 public class Service {
     //attributs
-    private String code;
-    private String nom;
-    private char batiment;
-    private int directeur;
-    private ArrayList<Employe> employes;
+    protected String code;
+    protected String nom;
+    protected String batiment;
+    protected String directeur;
+    protected String nom_directeur;
+    protected String prenom_directeur;
     //getters
     public String getCode(){return code;}
     public String getNom(){return nom;}
-    public char getBatiment(){return batiment;}
-    public int getDirecteur(){return directeur;}
-    //public ArrayList getEmploye(){return employes;}
+    public String getBatiment(){return batiment;}
+    public String getDirecteur(){return directeur;}
+    public String getNom_directeur(){return nom_directeur;}
+    public String getPrenom_directeur(){return prenom_directeur;}
+    
+    public Service(String code,String nom,String bâtiment,String directeur,String nom_directeur,String prenom_directeur){
+        this.code=code;
+        this.nom=nom;
+        this.batiment=bâtiment;
+        this.directeur=directeur;
+        this.nom_directeur=nom_directeur;
+        this.prenom_directeur=prenom_directeur;
+    }
 }
